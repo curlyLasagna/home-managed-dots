c08b43e22c21bc25d89c35e084e00daed5f5fa8a
luis <luis.gcodes@gmail.com>
2025-06-03 19:17:20 -0400

    update packages. Update git config

--- home.nix
+++ home.nix
@@ -16,7 +16,7 @@
     enable = true;
     configFile = {
       "nvim" = {
-        source = config.lib.file.mkOutOfStoreSymlink /Users/luis/.config/home-manager/dots/nvim;
+        source = config.lib.file.mkOutOfStoreSymlink ./dots/nvim;
       };
       # Download alacritty theme from alacritty-themes
       "alacritty/theme.toml" = {
@@ -188,8 +188,51 @@
       enable = true;
       userName = "luis";
       userEmail = "luis.gcodes@gmail.com";
+      aliases = {
+        l = "log --graph --decorate --pretty=format:'%C(auto)%h %Cblue%ad %Cred%aN %C(auto)%d %n    %s' --date=human";
+      };
+      ignores = [
+        "# Compiled source #"
+        "###################"
+        "*.com"
+        "*.class"
+        "*.dll"
+        "*.exe"
+        "*.o"
+        "*.so"
+        "# OS generated files #"
+        "######################"
+        ".DS_Store"
+        ".DS_Store?"
+        "._*"
+        ".Spotlight-V100"
+        ".Trashes"
+        "ehthumbs.db"
+        "Thumbs.db"
+        "# Packages #"
+        "############"
+        "*.7z"
+        "*.dmg"
+        "*.gz"
+        "*.iso"
+        "*.jar"
+        "*.rar"
+        "*.tar"
+        "*.zip"
+        "# Logs and databases #"
+        "######################"
+        "*.log"
+        "*.sqlite"
+        "*.retry"
+      ];
       extraConfig = {
         init.defaultBranch = "main";
+        core = {
+          autocrlf = false;
+        };
+        credential.helper = "osxkeychain";
+        github.user = "curlyLasagna";
+        branch.autoSetupMerge = "always";
       };
       lfs = {
         enable = true;
