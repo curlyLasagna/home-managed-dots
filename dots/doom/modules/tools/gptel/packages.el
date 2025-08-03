5fd76f18d469e607f8fec2bb8d148d29d2c63d34
luis <luis.gcodes@gmail.com>
2025-08-03 11:45:56 -0400

    forgor to stage lol

---
+++ dots/doom/modules/tools/gptel/packages.el
@@ -0,0 +1,25 @@
+;;; packages.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Luis Dale Gascon
+;;
+;; Author: Luis Dale Gascon <luis.gcodes@gmail.com>
+;; Maintainer: Luis Dale Gascon <luis.gcodes@gmail.com>
+;; Created: August 02, 2025
+;; Modified: August 02, 2025
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex text tools unix vc wp
+;; Homepage: https://github.com/curlyLasagna/packages
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+(package! gptel :recipe (:nonrecursive t))
+
+
+(provide 'packages)
+;;; packages.el ends here
