be35f9eb96fa21eedd8a4f2a02b114ad91c989a1
luis <luis.gcodes@gmail.com>
2025-08-07 14:37:18 -0400

    add slack

--- brew.sh
+++ brew.sh
@@ -3,15 +3,19 @@
 # Might as well
 sudo xcode-select --install
 
-# Install brew
+# Install brew if you can't
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Add homebrew to PATH to be able to run the commands below
+eval $(/opt/homebrew/bin/brew shellenv)
+
+# If brew is already installed, remove whatever is already installed
+brew remove --force $(brew list --formula)
 brew update
 brew upgrade
 
 # Install brew packages
 brew bundle install
-
 
 # Install nix :)
 curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate
--- work.nix
+++ work.nix
@@ -10,6 +10,7 @@
 {
   home.packages = with pkgs; [
     mongodb-compass
+    slack
 
     # ops
     awscli2
