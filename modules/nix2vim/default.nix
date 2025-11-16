7e90ad309e86c9b9332275464ee173fd83d20e8f
luis <luis.gcodes@gmail.com>
2025-11-16 14:28:33 -0500

    nix2vim config is a side project atp

--- modules/nix2vim/default.nix
+++ modules/nix2vim/default.nix
@@ -13,6 +13,7 @@
       ];
       expandtab = true;
       shiftwidth = 2;
+      tabstop = 2;
       # Highlight the current line for cursor
       cursorline = true;
 
@@ -21,27 +22,51 @@
 
       # Enable mouse
       mouse = "a";
+      mousemodel = "extend";
 
       # Global substitution by default
       gdefault = true;
       # Search
       ignorecase = true;
       smartcase = true;
-
+      autoindent = true;
     };
     colorschemes = {
       tokyonight.enable = true;
     };
     clipboard.register = "unnamedplus";
+    # keymaps = [
+    #   {
+    #     mode = ["n"];
+    #   }
+    # ];
     plugins = {
+      web-devicons.enable = true;
       which-key.enable = true;
       lualine.enable = true;
       oil.enable = true;
       nvim-autopairs.enable = true;
-      telescope.enable = true;
+      telescope = {
+        enable = true;
+        keymaps = {
+          "<leader>ff" = "find_files";
+          "<leader>fr" = "oldfiles";
+          "<leader>bb" = "buffers";
+          "<leader>/" = "live_grep";
+          "gd" = "lsp_definitions";
+          "gD" = "lsp_references";
+        };
+      };
       typst-vim.enable = true;
       typst-preview.enable = true;
-      lsp.enable = true;
+      lsp = {
+        enable = true;
+        inlayHints = true;
+        servers = {
+          nil_ls.enable = true;
+        };
+      };
+      lsp-format.enable = true;
       trouble.enable = true;
     };
     autoCmd = [
