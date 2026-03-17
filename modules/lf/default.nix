4dbedfcf7d67c755a5c3c5256d623019748eeb1d
luis <luis.gcodes@gmail.com>
2026-03-16 21:41:37 -0400

    idk how to use lf

--- modules/lf/default.nix
+++ modules/lf/default.nix
@@ -6,26 +6,19 @@
 }:
 
 with lib;
-let cfg = config.modules.lf;
-in {
+let
+  cfg = config.modules.lf;
+in
+{
   options.modules.lf = {
     enable = mkEnableOption "lf file manager";
   };
 
   config = mkIf cfg.enable {
-  programs = {
-    lf = {
-      enable = true;
-    };
-    fish = {
-      functions = {
-        lfcd = {
-          wraps = "lf";
-          description = "lf - Terminal file manager (changing directory on exit)";
-          body = ''cd "$(command lf -print-last-dir $argv)'';
-        };
+    programs = {
+      lf = {
+        enable = true;
       };
     };
   };
-};
 }
