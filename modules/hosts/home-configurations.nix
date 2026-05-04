5219d99b828246c845cb391c4b504be363f7c9f3
luis <luis.gcodes@gmail.com>
2026-05-04 14:59:31 -0400

    remove custom mkHome function

--- modules/hosts/home-configurations.nix
+++ modules/hosts/home-configurations.nix
@@ -3,95 +3,69 @@
   flake = {
     homeConfigurations =
       let
-        mkHome =
-          {
-            name,
-            system,
-            extraModules ? [ ],
-            overlays ? [ ],
-            allowUnfree ? true,
-          }:
-          let
-            pkgs = import inputs.nixpkgs {
-              inherit system overlays;
-              config.allowUnfree = allowUnfree;
-            };
-          in
-          {
-            ${name} = inputs.home-manager.lib.homeManagerConfiguration {
-              inherit pkgs;
-              modules = extraModules;
-            };
+        hm = inputs.self.modules.homeManager;
+        emacsOverlay = inputs.emacs-lsp-booster.overlays.default;
+
+        profiles = rec {
+          common = with hm; [
+            base
+            zsh
+            fzf
+            helix
+            zoxide
+            eza
+            fish
+            git
+            starship
+            lazygit
+            nixvim
+            doomemacs
+            lf
+          ];
+
+          darwin = common ++ [
+            hm.macos
+            hm.ghostty
+          ];
+        };
+
+        hosts = {
+          macbook = {
+            system = "aarch64-darwin";
+            overlays = [ emacsOverlay ];
+            aspects = profiles.darwin ++ [ hm.personal ];
           };
+
+          secured-macbook = {
+            system = "aarch64-darwin";
+            overlays = [ emacsOverlay ];
+            aspects = profiles.darwin ++ [ hm.work ];
+          };
+
+          wsl = {
+            system = "x86_64-linux";
+            aspects = profiles.common;
+          };
+        };
       in
-      { }
-      // (mkHome {
-        name = "macbook";
-        system = "aarch64-darwin";
-        overlays = [ inputs.emacs-lsp-booster.overlays.default ];
-        extraModules = with inputs.self.modules.homeManager; [
-          base
-          direnv
-          zsh
-          fzf
-          helix
-          zoxide
-          eza
-          macos
-          personal
-          fish
-          git
-          starship
-          lazygit
-          nixvim
-          doomemacs
-          lf
-          ghostty
-        ];
-      })
-      // (mkHome {
-        name = "secured-macbook";
-        system = "aarch64-darwin";
-        overlays = [ inputs.emacs-lsp-booster.overlays.default ];
-        extraModules = with inputs.self.modules.homeManager; [
-          base
-          direnv
-          zsh
-          fzf
-          helix
-          zoxide
-          eza
-          macos
-          work
-          fish
-          git
-          starship
-          lazygit
-          nixvim
-          doomemacs
-          lf
-          ghostty
-        ];
-      })
-      // (mkHome {
-        name = "wsl";
-        system = "x86_64-linux";
-        extraModules = with inputs.self.modules.homeManager; [
-          base
-          direnv
-          zsh
-          fzf
-          helix
-          zoxide
-          eza
-          fish
-          git
-          starship
-          lazygit
-          nixvim
-          doomemacs
-          lf
-        ];
-      });
+      builtins.mapAttrs (
+        _name:
+        {
+          system,
+          aspects,
+          overlays ? [ ],
+          allowUnfree ? true,
+        }:
+        let
+          pkgs = import inputs.nixpkgs {
+            inherit system overlays;
+            config.allowUnfree = allowUnfree;
+          };
+        in
+        inputs.home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = aspects;
+        }
+      ) hosts;
   };
 }
