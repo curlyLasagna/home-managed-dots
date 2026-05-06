0b00c6e46b9be928c05485409481470825890611
luis <luis.gcodes@gmail.com>
2026-05-06 12:37:37 -0400

    new zed home-manager config looks promising

--- modules/zed-editor/default.nix
+++ modules/zed-editor/default.nix
@@ -3,147 +3,155 @@
   flake.modules.homeManager.zed-editor =
     { config, pkgs, ... }:
     let
-      # Zed settings from https://gist.github.com/curlyLasagna/4e9e26b05122d41558da286d728d4d8e
-      userSettings = {
-        preview_tabs = {
-          enabled = true;
-        };
+      editorUiSettings = {
+        preview_tabs.enabled = false;
         show_whitespaces = "trailing";
-        inlay_hints = {
-          enabled = true;
+        inlay_hints.enabled = true;
+
+        icon_theme = {
+          light = "Catppuccin Latte";
+          dark = "Catppuccin Mocha";
         };
-        icon_theme = "Catppuccin Mocha";
-        theme = "Catppuccin Mocha";
+        theme = {
+          light = "Kanagawa Lotus";
+          dark = "Catppuccin Mocha";
+        };
+
         indent_guides = {
           enabled = true;
           coloring = "indent_aware";
         };
         soft_wrap = "editor_width";
-        scrollbar = {
-          show = "auto";
-        };
+        scrollbar.show = "auto";
+
         cursor_blink = false;
         relative_line_numbers = "enabled";
+
         ui_font_size = 15;
         ui_font_family = ".ZedSans";
         buffer_font_family = "RobotoMono Nerd Font Mono";
         buffer_font_size = 13.0;
-        buffer_font_features = {
-          liga = true;
-        };
+        buffer_font_features.liga = true;
+      };
+
+      formattingSettings = {
         format_on_save = "on";
         formatter = "language_server";
-        vim_mode = false;
+      };
+
+      vimSettings = {
+        vim_mode = true;
         vim = {
           use_system_clipboard = "always";
           toggle_relative_line_numbers = true;
         };
-        git_panel = {
-          dock = "left";
-        };
-        project_panel = {
-          dock = "right";
-        };
-        git = {
-          inline_blame = {
-            delay_ms = 5000;
-          };
-        };
+      };
+
+      panelSettings = {
+        git_panel.dock = "left";
+        project_panel.dock = "right";
+
+        git.inline_blame.delay_ms = 5000;
+
         tabs = {
           file_icons = true;
           git_status = true;
           show_diagnostics = "errors";
         };
+
         tab_bar = {
           show_nav_history_buttons = false;
           show = true;
         };
+      };
+
+      terminalSettings = {
         terminal = {
-          shell = {
-            program = "fish";
-          };
+          shell.program = "fish";
           font_family = "ZedMono Nerd Font Mono";
           font_size = 13;
           option_as_meta = true;
           working_directory = "current_project_directory";
         };
+      };
+
+      languageSettings = {
         languages = {
           Python = {
-            formatter = {
-              language_server = {
-                name = "ruff";
-              };
-            };
+            formatter.language_server.name = "ruff";
             language_servers = [
               "pyright"
               "ruff"
             ];
           };
-          Nix = {
-            language_servers = [ "nil" ];
-          };
+
+          Nix.language_servers = [ "nil" ];
         };
+      };
+
+      lspSettings = {
         lsp = {
+          nixd = {
+            settings = {
+              diagnostic = {
+                suppress = [ "sema-extra-with" ];
+              };
+            };
+          };
           vtsls = {
             settings = {
-              typescript = {
-                updateImportsOnFileMove = {
-                  enabled = "always";
-                };
-              };
-              javascript = {
-                updateImportsOnFileMove = {
-                  enabled = "always";
-                };
-              };
+              typescript.updateImportsOnFileMove.enabled = "always";
+              javascript.updateImportsOnFileMove.enabled = "always";
             };
             enable_lsp_tasks = true;
           };
-          pyright = {
-            settings = {
-              pyright.analysis = {
-                diagnosticMode = "workspace";
-                typeCheckingMode = "off";
-              };
-            };
+
+          pyright.settings.pyright.analysis = {
+            diagnosticMode = "workspace";
+            typeCheckingMode = "off";
           };
-          tinymist = {
-            settings = {
-              exportPdf = "onSave";
-              outputPath = "$root/$name";
-            };
+
+          tinymist.settings = {
+            exportPdf = "onSave";
+            outputPath = "$root/$name";
           };
         };
+      };
+
+      predictionSettings = {
         edit_predictions = {
           provider = "none";
           mode = "subtle";
         };
+      };
+
+      agentSettings = {
         agent = {
           default_profile = "ask";
-          profiles = {
-            edit = {
-              name = "Edit";
-              tools = {
-                diagnostics = true;
-                read_file = true;
-                create_directory = false;
-                edit_file = true;
-              };
-              enable_all_context_servers = false;
-              context_servers = { };
+          profiles.edit = {
+            name = "Edit";
+            tools = {
+              diagnostics = true;
+              read_file = true;
+              create_directory = false;
+              edit_file = true;
             };
+            enable_all_context_servers = false;
+            context_servers = { };
           };
           single_file_review = true;
-          default_model = {
-            provider = "copilot_chat";
-            model = "gpt-4.1";
-          };
           dock = "left";
         };
+      };
+
+      telemetrySettings = {
         telemetry = {
           diagnostics = false;
           metrics = false;
         };
+      };
+
+      fileScanSettings = {
         file_scan_exclusions = [
           "**/.git"
           "**/.svn"
@@ -168,7 +176,22 @@
         ];
       };
 
-      # Zed keymaps from https://gist.github.com/curlyLasagna/36a7d3e0466eccd198ae089292b55ba9
+      userSettings = {
+        # Misc. settings
+        cli_default_open_behavior = "new_window";
+      }
+      // editorUiSettings
+      // formattingSettings
+      // vimSettings
+      // panelSettings
+      // terminalSettings
+      // languageSettings
+      // lspSettings
+      // predictionSettings
+      // agentSettings
+      // telemetrySettings
+      // fileScanSettings;
+
       userKeymaps = [
         {
           context = "Editor && vim_mode == normal && vim_operator == none && !VimWaiting";
@@ -177,6 +200,7 @@
             "space f f" = "file_finder::Toggle";
             "space t z" = "workspace::ToggleCenteredLayout";
             "space w s" = "pane::SplitHorizontal";
+            "space t w" = "workspace::ToggleZoom";
             "space w v" = "pane::SplitVertical";
             "space b d" = "pane::CloseActiveItem";
             "space w d" = [
