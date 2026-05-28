d251894b2503abe6c8f17a35a2f7340b39085d71
luis <luis.gcodes@gmail.com>
2026-05-28 19:51:56 -0400

    One too many parens

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,9 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
- ((nix-mode
-    . ((eglot-workspace-configuration
-        . (:nixd
-           (:options
-            (:home-manager
-             (:expr "(builtins.getFlake (builtins.toString ./.)).homeConfigurations.\"personal-macbook\".options")))))))))
+((nix-mode
+  . ((eglot-workspace-configuration
+      . (:nixd
+         (:options
+          (:home-manager
+           (:expr "(builtins.getFlake (builtins.toString ./.)).homeConfigurations.\"personal-macbook\".options"))))))))
