28134f6f74b6796d2f0747077301f85ac55c7e73
Luis <luis.gcodes@gmail.com>
2026-06-22 11:25:33 -0400

    vanilla emacs config wip

--- modules/editor/emacs/minimal/config/post-init.el
+++ modules/editor/emacs/minimal/config/post-init.el
@@ -1,16 +1,27 @@
+(setq user-emacs-directory (expand-file-name "~/.config/minimal-emacs/"))
+(setq package-user-dir (expand-file-name "elpa/" user-emacs-directory))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+(when (fboundp 'startup-redirect-eln-cache)
+  (startup-redirect-eln-cache (expand-file-name "eln-cache/" user-emacs-directory)))
+
+(use-package use-package
+  :custom
+  (use-package-always-ensure t)
+  (package-native-compile nil))
+
+
 (use-package exec-path-from-shell
+  ; Only apply to macOS
   :if (and (or (display-graphic-p) (daemonp))
-           (eq system-type 'darwin)) ; macOS only
+           (eq system-type 'darwin)) 
   :demand t
   :functions exec-path-from-shell-initialize
   :config
-  (dolist (var '("TMPDIR"
-                 "SSH_AUTH_SOCK" "SSH_AGENT_PID"
-                 "GPG_AGENT_INFO"
-                 "LANG" "LC_CTYPE"))
-    (add-to-list 'exec-path-from-shell-variables var))
-  ;; Initialize
   (exec-path-from-shell-initialize))
+
+(setq native-comp-jit-compilation t)
+(setq native-comp-async-report-warnings-errors nil)
 
 ;; Auto-revert in Emacs is a feature that automatically updates the
 ;; contents of a buffer to reflect changes made to the underlying file
@@ -91,7 +102,7 @@
 ;; by providing additional backends through completion-at-point-functions.
 (use-package cape
   :commands (cape-dabbrev cape-file cape-elisp-block)
-  ; :bind ("C-c p" . cape-prefix-map)
+                                        ; :bind ("C-c p" . cape-prefix-map)
   :init
   ;; Add to the global default value of `completion-at-point-functions' which is
   ;; used by `completion-at-point'.
@@ -105,8 +116,8 @@
              undo-fu-only-redo
              undo-fu-only-redo-all
              undo-fu-disable-checkpoint))
-  ; :config
-  ; (global-unset-key (kbd "C-z"))
+                                        ; :config
+                                        ; (global-unset-key (kbd "C-z"))
 
 ;; The undo-fu-session package complements undo-fu by enabling the saving
 ;; and restoration of undo history across Emacs sessions, even after restarting.
@@ -136,17 +147,71 @@
   :commands (org-mode org-version)
   :mode
   ("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode)
+  :config
+  (setq +org-capture-journal-file (expand-file-name "journal.org" org-directory))
+  (setq +org-capture-projects-file (expand-file-name "projects.org" org-directory))
   :custom
   (org-hide-leading-stars t)
   (org-startup-indented t)
   (org-adapt-indentation nil)
   (org-edit-src-content-indentation 0)
   (org-indent-mode 1)
-  ;; (org-fontify-done-headline t)
-  ;; (org-fontify-todo-headline t)
-  ;; (org-fontify-whole-heading-line t)
-  ;; (org-fontify-quote-and-verse-blocks t)
-  (org-startup-truncated t))
+  (org-fontify-todo-headline t)
+  (org-fontify-quote-and-verse-blocks t)
+  (org-startup-truncated t)
+  (org-directory (expand-file-name "~/Documents/org"))
+
+  (org-capture-templates
+        ;; Vanilla org is used for project, journals and meetings
+        '(
+          ("j" "Journal")
+          ("jn" "Journal" entry
+           (file+olp+datetree +org-capture-journal-file)
+           "* %<%I:%M %p>\n%?" :prepend t :tree-type (year month day)
+           )
+
+          ("jd" "Journal on date" entry
+           (file+olp+datetree +org-capture-journal-file)
+           "* %<%I:%M %p>\n%?"
+           :prepend t :time-prompt t :tree-type (year month day)
+           )
+
+          ("jw" "Work log" entry
+           (file+olp+datetree "work-log.org")
+           "* %?" :clock-in t :clock-keep t :tree-type (year month week day)
+           )
+
+          ("p" "Projects")
+          ("pN" "New project" entry
+           (file +org-capture-projects-file)
+           "* %^{Project Name}\n** Tasks\n** Notes")
+          ("pt" "Project todo" entry
+           (function my/org-capture-select-project-target)
+           "* TODO %? %i"
+           :subheading "Tasks"
+           :prepend nil)
+
+          ("pn" "Project notes" entry
+           (function my/org-capture-select-project-target)
+           "* %? %i"
+           :subheading "Notes"
+           :prepend t)
+          )
+        )
+  (org-todo-keywords
+   '(
+          ;; Project todos
+          (sequence "TODO" "WIP" "DONE")
+          ;; Note-taking todos
+          (sequence "INBOX(i)"
+	            "PROCESSING(p)"
+	            "WAIT(w)"
+	            "TO-READ(r)"
+	            "|"
+	            "DONE(d)"
+	            ))
+   )
+  )
 
 (use-package elec-pair
   :ensure nil
@@ -160,61 +225,157 @@
   :mode ("\\.nix\\'" . nix-mode))
 
 (use-package no-littering
-   :config
-   (no-littering-theme-backups))
+  :init
+  (eval-and-compile
+    (setq no-littering-etc-directory
+          (expand-file-name "etc/" "~/.config/minimal-emacs/"))
+    (setq no-littering-var-directory
+          (expand-file-name "var/" "~/.config/minimal-emacs/")))
+  :config
+  (no-littering-theme-backups))
 
 (use-package consult
-   :bind (
-          ;; Drop-in replacements
-          ("C-x b" . consult-buffer)
-          ("M-y"   . consult-yank-pop)
-          ;; Searching
-          ("M-s /" . consult-ripgrep)
-   )
-)
+  :bind (
+         ("C-x b" . consult-buffer)
+         ("M-y"   . consult-yank-from-kill-ring)
+         ("C-c s i" . consult-imenu)
+         )
+  )
 
 (use-package dired
   :ensure nil
   :commands (dired)
   :hook
-  ((dired-mode . dired-hide-details-mode)
-   (dired-mode . hl-line-mode))
+  (
+   (dired-mode . hl-line-mode)
+  )
   :config
   (setq dired-recursive-copies 'always)
   (setq dired-recursive-deletes 'always)
   (setq delete-by-moving-to-trash t)
+  (setq dired-kill-when-opening-new-dired-buffer t)
   (setq dired-dwim-target t))
 
 (use-package dired-subtree
-   :after dired
-   :bind
-   ( :map dired-mode-map
-     ("<tab>" . dired-subtree-toggle)
-     ("TAB" . dired-subtree-toggle)
-     ("<backtab>" . dired-subtree-remove)
-     ("S-TAB" . dired-subtree-remove))
-   :config
-   (setq dired-subtree-use-backgrounds nil))
+  :after dired
+  :bind
+  ( :map dired-mode-map
+    ("<tab>" . dired-subtree-toggle)
+    ("TAB" . dired-subtree-toggle)
+    ("<backtab>" . dired-subtree-remove)
+    ("S-TAB" . dired-subtree-remove))
+  :config
+  (setq dired-subtree-use-backgrounds nil))
 
 (use-package orderless
-    :custom
-    (completion-styles '(orderless basic))
-    ;; Do fuzzy search by adding orderless-flex to the list.
-    (orderless-matching-styles '(orderless-literal orderless-regexp))
-    ;; Get orderless everywhere
-    (completion-category-defaults nil)
-    (completion-category-overrides
-     ;; from Emacs Writing Studio & Corfu README
-     '((file (styles partial-completion)))))
+  :custom
+  (completion-styles '(orderless basic))
+  ;; Do fuzzy search by adding orderless-flex to the list.
+  (orderless-matching-styles '(orderless-literal orderless-regexp))
+  ;; Get orderless everywhere
+  (completion-category-defaults nil)
+  (completion-category-overrides
+   ;; from Emacs Writing Studio & Corfu README
+   '((file (styles partial-completion)))))
 
 (use-package dumb-jump
-   :hook (xref-backend-functions . dumb-jump-xref-activate)
-   :custom
-   ;; use consult instead of pop-up buffer
-   (xref-show-definitions-function 'xref-show-definitions-completing-read)
-   )
+  :hook (xref-backend-functions . dumb-jump-xref-activate)
+  :custom
+  ;; use consult instead of pop-up buffer
+  (xref-show-definitions-function 'xref-show-definitions-completing-read)
+  )
 
+(use-package magit
+  :custom
+  ;; Improve readability of diffs
+  (magit-diff-refine-hunk 'all)
+  )
+
+(use-package auto-dark
+  :custom
+  (auto-dark-themes '((modus-vivendi-tinted) (modus-operandi-tinted)))
+  :config
+  (auto-dark-mode))
+
+(defun prot/keyboard-quit-dwim ()
+  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+  The generic `keyboard-quit' does not do the expected thing when
+  the minibuffer is open.  Whereas we want it to close the
+  minibuffer, even without explicitly focusing it.
+
+  The DWIM behaviour of this command is as follows:
+
+  - When the region is active, disable it.
+  - When a minibuffer is open, but not focused, close the minibuffer.
+  - When the Completions buffer is selected, close it.
+  - In every other case use the regular `keyboard-quit'."
+  (interactive)
+  (cond
+   ((region-active-p)
+    (keyboard-quit))
+   ((derived-mode-p 'completion-list-mode)
+    (delete-completion-window))
+   ((> (minibuffer-depth) 0)
+    (abort-recursive-edit))
+   (t
+    (keyboard-quit))))
+
+(global-set-key [remap keyboard-quit] 'prot/keyboard-quit-dwim)
+
+(use-package stripspace
+  ;; Enable for prog-mode-hook, text-mode-hook, conf-mode-hook
+  :hook ((prog-mode . stripspace-local-mode)
+         (text-mode . stripspace-local-mode)
+         (conf-mode . stripspace-local-mode))
+
+  :custom
+  ;; The `stripspace-only-if-initially-clean' option:
+  ;; - nil to always delete trailing whitespace.
+  ;; - Non-nil to only delete whitespace when the buffer is clean initially.
+  ;; (The initial cleanliness check is performed when `stripspace-local-mode'
+  ;; is enabled.)
+  (stripspace-only-if-initially-clean nil)
+
+  ;; Enabling `stripspace-restore-column' preserves the cursor's column position
+  ;; even after stripping spaces. This is useful in scenarios where you add
+  ;; extra spaces and then save the file. Although the spaces are removed in the
+  ;; saved file, the cursor remains in the same position, ensuring a consistent
+  ;; editing experience without affecting cursor placement.
+  (stripspace-restore-column t))
+
+(use-package denote
+  :hook (dired-mode . denote-dired-mode)
+  :bind
+  (("C-c n n" . denote)
+   ("C-c n r" . denote-rename-file)
+   ("C-c n l" . denote-link)
+   ("C-c n b" . denote-backlinks)
+   ("C-c n d" . denote-dired)
+   ("C-c n g" . denote-grep))
+  :config
+  (setq denote-directory (expand-file-name "denotes" org-directory))
+  (denote-rename-buffer-mode 1))
+
+(defun indent-whole-buffer ()
+  "Indent the entire buffer without affecting point or mark."
+  (interactive)
+  (save-excursion
+    (save-restriction
+      (indent-region (point-min) (point-max)))))
+
+(add-hook 'after-init-hook #'global-auto-revert-mode)
+(add-hook 'after-init-hook #'recentf-mode)
+(add-hook 'after-init-hook #'savehist-mode)
+(add-hook 'after-init-hook #'save-place-mode)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+(setq display-line-numbers-type 'relative)
+(setq display-line-numbers-width 3)
 (delete-selection-mode 1)
 (setq line-number-mode t)
 (setq confirm-kill-emacs 'y-or-n-p)
 (setq truncate-string-ellipsis "…")
+(flymake-mode 1)
+
+(set-face-attribute 'default nil :height 140)
