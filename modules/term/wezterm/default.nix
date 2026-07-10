f4c2e32c4ac8d37261dc4fd7cf65afa0ef4574e1
Luis <luis.gcodes@gmail.com>
2026-07-10 07:56:56 -0400

    set wezterm default shell

--- modules/term/wezterm/default.nix
+++ modules/term/wezterm/default.nix
@@ -5,9 +5,14 @@
     {
       programs.wezterm.enable = true;
 
-      # Template wezterm.lua with the correct shell
+      # Set to custom defaultShell. fish is fallback
       xdg.configFile."wezterm/wezterm.lua".text =
-        builtins.replaceStrings [ "fish" ] [ config.myHost.defaultShell ]
+        builtins.replaceStrings
+          # from `fish`
+          [ "fish" ]
+          # to whatever shell you say is your default shell
+          [ config.myHost.defaultShell ]
+          # wezterm file
           (builtins.readFile ./wezterm.lua);
       xdg.configFile."wezterm/config/init.lua".source = ./config/init.lua;
     };
