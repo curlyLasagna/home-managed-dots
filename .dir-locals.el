1657e938047c249371ffd2c65abe263b2ec530b8
Luis <luis.gcodes@gmail.com>
2026-07-18 10:25:39 -0400

    fix dir locals el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,11 +1,16 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nix-mode
-  . ((eglot-workspace-configuration
-      . (:nixd
-         (:options
-          (:home-manager
-           (:expr "(builtins.getFlake (builtins.toString ./.)).homeConfigurations.\"personal-macbook\".options"))))))))
-
-((nil . ((projectile-project-compilation-cmd . "home-manager switch --flake .#"))))
+(
+ (nil
+  . ((projectile-project-compilation-cmd . "home-manager switch --flake .#")))
+ (nix-mode
+  . (
+     (eglot-workspace-configuration
+      .
+      (:nixd
+       (:options
+        (:home-manager
+         (:expr
+          "(builtins.getFlake (builtins.toString ./.)).homeConfigurations.\"personal-macbook\".options")))))))
+ )
