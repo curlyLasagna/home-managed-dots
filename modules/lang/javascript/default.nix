7d70c7ac541b9ed23a74e29da928ef8abe25598d
Luis <luis.gcodes@gmail.com>
2026-07-24 11:51:27 -0400

    js slop

--- modules/lang/javascript/default.nix
+++ modules/lang/javascript/default.nix
@@ -3,6 +3,10 @@
   flake.homeModules."javascript" =
     { pkgs, ... }:
     {
-      home.packages = [ pkgs.nodejs_24 ];
+      home.packages = [
+        pkgs.nodejs_24
+        pkgs.typescript-go
+        pkgs.vtsls
+      ];
     };
 }
